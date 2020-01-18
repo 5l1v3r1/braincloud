b39f6decea9be1aa59a9ca4fb5709fdfea6f5d21
muhammad hamza <muhammadhamza2965@gmail.com>
2020-01-17 20:46:57 -0800

    Changing port in app.js

--- app.js
+++ app.js
@@ -31,5 +31,9 @@ app.get('/userprofile',(req,res)=>{
 });
 
 
-var port = process.env.port || 3000;
-app.listen(port);
+app.set( 'port', ( process.env.PORT || 5000 ));
+
+// Start node server
+app.listen( app.get( 'port' ), function() {
+  console.log( 'Node server is running on port ' + app.get( 'port' ));
+  });
